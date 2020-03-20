b4e2d4d4fd2a27015993f09f23e745ce01d85673
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-03-21 04:41:08 +0900

    initial commit

---
+++ app/src/main/java/dev/honwaka_lab/generateretrofitfromopenapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.honwaka_lab.generateretrofitfromopenapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
